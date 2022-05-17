aae69c3269f607107bf1f909cf8e113e6eea3370
moet-stein <0101.mste@gmail.com>
2022-05-17 17:59:50 +0200

    main commit

---
+++ Werdd/Supporting Files/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Werdd
+//
+//  Created by Moe Steinmueller on 14.04.22.
+//
+
+import Foundation
+let WORDSAPI_KEY = "598e7abf2cmsh26c4f18bc6f13f9p1b8309jsnd187f9d0f4cc"
