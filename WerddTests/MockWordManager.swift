e2b896ae8bbba5c31f0c728ecc35a00adc317fde
moet-stein <0101.mste@gmail.com>
2022-06-01 22:18:21 +0200

    implement testFetchedInputWordSuccess

--- WerddTests/MockWordManager.swift
+++ WerddTests/MockWordManager.swift
@@ -39,34 +39,5 @@ final class MockWordManager: NetWorkingProtocol {
             }
         }
     }
-    
-    
-//    static var requestHandler: ((URLRequest) throws -> (HTTPURLResponse, Data))?
-//
-//        override class func canInit(with request: URLRequest) -> Bool {
-//            return true
-//        }
-//
-//        override class func canonicalRequest(for request: URLRequest) -> URLRequest {
-//            return request
-//        }
-//
-//        override func startLoading() {
-//            guard let handler = MockWordManager.requestHandler else {
-//                XCTFail("Received unexpected request with no handler set")
-//                return
-//            }
-//            do {
-//                let (response, data) = try handler(request)
-//                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
-//                client?.urlProtocol(self, didLoad: data)
-//                client?.urlProtocolDidFinishLoading(self)
-//            } catch {
-//                client?.urlProtocol(self, didFailWithError: error)
-//            }
-//        }
-//
-//        override func stopLoading() {
-//        }
 
 }
--- WerddTests/WordMangerTests.swift
+++ WerddTests/WordMangerTests.swift
@@ -22,7 +22,7 @@ class WordMangerTests: XCTestCase {
         homeVC = nil
     }
     
-    func testFetchedWordSuccess() {
+    func testFetchedRandomWordSuccess() {
         
         homeVC.wordManager.fetchRandomWord(completion: { result in
             switch result {
@@ -33,6 +33,26 @@ class WordMangerTests: XCTestCase {
                print("fail")
             }
         })
+    }
+    
+    func testFetchedInputWordSuccess() {
+        homeVC.wordManager.fetchInputWord(inputWord: "swift") { result in
+            switch result {
+            case .success(let word):
+                XCTAssertEqual(word.word, "swift")
+                
+                let results = [
+                    Result(definition: "moving very fast", partOfSpeech: "adjective", synonyms: ["fleet"], antonyms: nil, examples: ["a swift current", "swift flight of an arrow", "a swift runner"]),
+                    Result(definition: "common western lizard; seen on logs or rocks", partOfSpeech: "noun", synonyms: ["blue-belly", "sceloporus occidentalis", "western fence lizard"], antonyms: nil, examples: nil),
+                    Result(definition: "United States meat-packer who began the use of refrigerated railroad cars (1839-1903)", partOfSpeech: "noun", synonyms: ["gustavus franklin swift"], antonyms: nil, examples: nil),
+                    Result(definition: "an English satirist born in Ireland (1667-1745)", partOfSpeech: "noun", synonyms: ["dean swift", "jonathan swift"], antonyms: nil, examples: nil),
+                    Result(definition: "a small bird that resembles a swallow and is noted for its rapid flight", partOfSpeech: "noun", synonyms: nil, antonyms: nil, examples: nil)
+                ]
+                XCTAssertEqual(word.results, results)
+            case .failure:
+               print("fail")
+            }
+        }
     }
 
 }
