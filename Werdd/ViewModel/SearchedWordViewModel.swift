821577b8a2aaee069b6ee7b25bcf9a7a4c04c572
moet-stein <0101.mste@gmail.com>
2022-06-01 11:28:15 +0200

    create randomWordVC and WordsTableVC

---
+++ Werdd/ViewModel/SearchedWordViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  SearchedWordViewModel.swift
+//  Werdd
+//
+//  Created by Moe Steinmueller on 01.06.22.
+//
+
+import Foundation
+
+//struct SearchedWordViewModel {
+//    let word: String
+//
+//    init(n)
+//}
